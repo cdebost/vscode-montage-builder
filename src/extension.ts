691d9ac80554f9445de2dd4101857a222b793589
Corentin Debost <codebost@gmail.com>
2017-12-22 19:30:35 -0800

    Rename extension to Montage Builder

--- src/extension.ts
+++ src/extension.ts
@@ -6,26 +6,11 @@ import { ReelDocumentContentProvider } from './reel-document-content-provider';
 
 export function activate(context: vscode.ExtensionContext) {
     const provider = new ReelDocumentContentProvider(context);
-    const providerRegistrations = vscode.workspace.registerTextDocumentContentProvider(ReelDocumentContentProvider.scheme, provider);
 
-    const commandRegistration = vscode.commands.registerCommand('extension.reel', async () => {
-        if (!checkValidEditor()) {
-            return;
-        }
+    const providerRegistrations = vscode.workspace.registerTextDocumentContentProvider(
+        ReelDocumentContentProvider.scheme, provider);
 
-        const filename = vscode.window.activeTextEditor.document.fileName;
-        const uri = vscode.Uri.parse(
-            ReelDocumentContentProvider.scheme +
-            '://' +
-            encodeURIComponent(filename)
-        );
-
-        try {
-            await vscode.commands.executeCommand('vscode.previewHtml', uri);
-        } catch (err) {
-            vscode.window.showErrorMessage(err);
-        }
-    });
+    const commandRegistration = vscode.commands.registerCommand('builder.open', openBuilder);
 
     context.subscriptions.push(provider, providerRegistrations, commandRegistration);
 }
@@ -34,7 +19,26 @@ export function activate(context: vscode.ExtensionContext) {
 export function deactivate() {
 }
 
-function checkValidEditor(): boolean {
+async function openBuilder() {
+    if (!checkValidEditor()) {
+        return;
+    }
+
+    const filename = vscode.window.activeTextEditor.document.fileName;
+    const uri = vscode.Uri.parse(
+        ReelDocumentContentProvider.scheme +
+        '://' +
+        encodeURIComponent(filename)
+    );
+
+    try {
+        await vscode.commands.executeCommand('vscode.previewHtml', uri);
+    } catch (err) {
+        vscode.window.showErrorMessage(err);
+    }
+}
+
+function checkValidEditor() : boolean {
     if (!vscode.window.activeTextEditor) {
         vscode.window.showErrorMessage("No document selected.");
         return false;
